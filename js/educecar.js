a618c2fced5ec9d90eb0572c84b4a771bdcf9845
jorge.artuz <jorge.artuz@cecar.edu.co>
2023-10-09 21:19:07 -0500

    Proyecto base

--- js/educecar.js
+++ js/educecar.js
@@ -32,3 +32,39 @@ function iniciarSesion(usuario, contraseña) {
 // Ejemplo de inicio de sesión
 let mensajeInicioSesion = iniciarSesion("juan123", "contraseña123");
 console.log(mensajeInicioSesion);
+
+
+
+// Obtener todos los botones de matrícula
+const botonesMatricular = document.querySelectorAll(".matricular");
+
+// Función para manejar la matrícula en un curso
+function matricularEnCurso() {
+    // Aquí puedes agregar la lógica de matrícula, como agregar el usuario al curso, registrar la fecha, etc.
+    alert("¡Te has matriculado en este curso!");
+}
+
+// Agregar un evento de clic a cada botón de matrícula
+botonesMatricular.forEach((boton) => {
+    boton.addEventListener("click", matricularEnCurso);
+});
+
+
+
+
+
+
+// Obtener todas las secciones de matrícula
+const seccionesMatricula = document.querySelectorAll(".matricula");
+
+// Función para mostrar detalles de matrícula
+function mostrarDetallesMatricula(event) {
+    const seccion = event.currentTarget;
+    // Aquí puedes agregar la lógica para mostrar detalles de matrícula, como obtener datos de matrícula desde tu base de datos y mostrarlos en la página.
+    // Por ejemplo, puedes usar alertas, modales o actualizar el contenido de la sección.
+}
+
+// Agregar un evento de clic a cada sección de matrícula
+seccionesMatricula.forEach((seccion) => {
+    seccion.addEventListener("click", mostrarDetallesMatricula);
+});
