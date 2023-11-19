bb9c70ac92e425d389704ad5de3d942bf14cf8d4
Juanchosky <142184377+Juanchosky@users.noreply.github.com>
2023-11-18 19:24:57 -0500

    Corregido lo de bienvenida de instructor y admin
    Actualizaciones recientes:
    
    - Se esta arreglando actualmente lo de gestionar matricula para que funcione y se pueda enviar los cursos al estudiante correspondiente. 
    Este tiene dos versiones ya que está en correccion y esta es la primera para comparar codigos y ver donde esta el problema.

---
+++ PROYECTO/Js/PanelInstructor.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    mostrarNombreInstructor(); // Llama a la función para mostrar el nombre al cargar la página
+
+    // Puedes suscribir la función mostrarNombreEstudiante a eventos adicionales si es necesario.
+    // Ejemplo: document.addEventListener('nombreActualizado', mostrarNombreEstudiante);
+});
+
+function mostrarNombreInstructor() {
+    // Recuperar el nombre del estudiante desde localStorage
+    const loggedInUserData = JSON.parse(localStorage.getItem('logged_in_user_data'));
+    const loggedInUserName = loggedInUserData ? `${loggedInUserData.first_name} ${loggedInUserData.last_name}` : null;
+
+    // Mostrar el nombre en el panel de control
+    const welcomeMessage = document.querySelector('.content h2');
+    if (welcomeMessage && loggedInUserName) {
+        welcomeMessage.textContent = `Bienvenido, ${loggedInUserName}`;
+    }
+}
--- PROYECTO/Js/gestionMatriculas.js
+++ PROYECTO/Js/gestionMatriculas.js
@@ -1,82 +1,88 @@
-let matriculas = [];
-let temasDisponibles = []; 
-let estudiantesDisponibles = []; 
+document.addEventListener('DOMContentLoaded', function () {
+    // Verificar si el navegador soporta localStorage
+    if (typeof Storage === 'undefined') {
+        alert('Lo siento, tu navegador no admite el almacenamiento local.');
+        return;
+    }
 
+    // Obtener elementos del formulario
+    const formMatricula = document.getElementById('formMatricula');
+    const temasDisponibles = document.getElementById('temasDisponibles');
+    const estudiantesDisponibles = document.getElementById('estudiantesDisponibles');
+    const listaMatriculas = document.getElementById('matriculas');
 
+    // Cargar opciones de temas y estudiantes desde el localStorage (asume que ya los tienes almacenados)
+    const temas = JSON.parse(localStorage.getItem('temas')) || [];
+    const users = JSON.parse(localStorage.getItem('users')) || [];
 
-function realizarMatricula() {
-    
-    var fechaMatricula = document.getElementById('fechaMatricula').value;
-    var temaSeleccionado = document.getElementById('temasDisponibles').value;
-    var estudianteSeleccionado = document.getElementById('estudiantesDisponibles').value;
-    var valorMatricula = document.getElementById('valorMatricula').value;
+    // Obtener solo los usuarios de tipo 'Student' para cargar en el select de estudiantes
+    const estudiantes = users.filter(user => user.user_type === 'Student') || [];
 
-    var tablaMatriculas = document.getElementById('tablaMatriculas');
-    var newRow = tablaMatriculas.insertRow();
-
-    
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
-
-
-    cell2.innerHTML = fechaMatricula;
-    cell3.innerHTML = temaSeleccionado;
-    cell4.innerHTML = estudianteSeleccionado;
-    cell5.innerHTML = valorMatricula;
-
-    document.getElementById('formMatricula').reset();
-}
-
-
-
-function mostrarMatriculas() {
-    const matriculasSection = document.getElementById('matriculas');
-    matriculasSection.innerHTML = ''; 
-
-    matriculas.forEach(matricula => {
-        const matriculaDiv = document.createElement('div');
-        matriculaDiv.innerHTML = `<h3>Matrícula realizada por ${matricula.estudiante} en el tema ${matricula.tema}</h3>
-                             <p><strong>Fecha de Matrícula:</strong> ${matricula.fecha}</p>
-                             <p><strong>Valor de Matrícula:</strong> ${matricula.valor}</p>
-                             <hr>`;
-        matriculasSection.appendChild(matriculaDiv);
-    });
-}
-
-
-function llenarOpcionesFormulario() {
-    const selectTemas = document.getElementById('temasDisponibles');js
-    const selectEstudiantes = document.getElementById('estudiantesDisponibles');
-
-   
-    selectTemas.innerHTML = '';
-    selectEstudiantes.innerHTML = '';
-
-    
-    temasDisponibles.forEach(tema => {
-        const option = document.createElement('option');
-        option.value = tema.codigo;
-        option.text = tema.nombre;
-        selectTemas.appendChild(option);
-    });
-
-    
-    estudiantesDisponibles.forEach(estudiante => {
-        if (estudiante.tipoUsuario === 'Estudiante') {
+    // Función para cargar opciones en un elemento select
+    function cargarOpciones(elementoSelect, opciones) {
+        elementoSelect.innerHTML = '';
+        opciones.forEach((opcion) => {
             const option = document.createElement('option');
-            option.value = estudiante.usuario;
-            option.text = `${estudiante.nombres} ${estudiante.apellidos}`;
-            selectEstudiantes.appendChild(option);
+            option.value = opcion.codigo;
+            option.textContent = opcion.nombre; // Ajusta esto según la estructura de tus objetos
+            elementoSelect.appendChild(option);
+        });
+    }
+
+    // Cargar opciones de temas y estudiantes en los elementos select
+    cargarOpciones(temasDisponibles, temas);
+    cargarOpciones(estudiantesDisponibles, estudiantes);
+
+    // Función para mostrar la lista de matrículas
+    function mostrarListaMatriculas() {
+        const matriculas = JSON.parse(localStorage.getItem('matriculas')) || [];
+        listaMatriculas.innerHTML = '';
+        matriculas.forEach((matricula) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `<p>Fecha: ${matricula.fecha}, Tema: ${matricula.temaId}, Estudiante: ${matricula.estudianteId}, Valor: ${matricula.valor}</p>`;
+            listaMatriculas.appendChild(listItem);
+        });
+    }
+
+    // Función para realizar una nueva matrícula
+    function realizarMatricula() {
+        // Obtener los datos necesarios del formulario de matrícula
+        const fechaMatricula = document.getElementById('fechaMatricula').value;
+        const temaSeleccionado = temas.find((tema) => tema.codigo === temasDisponibles.value); // Usar el campo apropiado para temas
+        const estudianteSeleccionado = users.find((user) => user.codigo === estudiantesDisponibles.value); // Usar el campo apropiado para estudiantes
+        const valorMatricula = document.getElementById('valorMatricula').value;
+    
+        // Validar que se haya seleccionado un tema y un estudiante
+        if (!temaSeleccionado || !estudianteSeleccionado) {
+            alert('Por favor, selecciona un tema y un estudiante.');
+            return;
         }
-    });
-}
 
+        // Realizar la matrícula (agregarla al objeto de matrículas)
+        const matriculas = JSON.parse(localStorage.getItem('matriculas')) || [];
+        const nuevaMatricula = {
+            id: matriculas.length + 1, // Generar un nuevo ID (ajusta esto según tu lógica)
+            fecha: fechaMatricula,
+            temaId: temaSeleccionado.codigo, // Usar el campo apropiado para temas
+            estudianteId: estudianteSeleccionado.codigo, // Usar el campo apropiado para estudiantes
+            valor: parseFloat(valorMatricula), // Ajusta esto según la estructura de tus objetos
+        };
 
-llenarOpcionesFormulario();
+        matriculas.push(nuevaMatricula);
 
+        // Guardar el objeto actualizado en el localStorage
+        localStorage.setItem('matriculas', JSON.stringify(matriculas));
 
-mostrarMatriculas();
+        // Mostrar la lista actualizada de matrículas
+        mostrarListaMatriculas();
 
+        // Limpiar el formulario después de realizar la matrícula
+        formMatricula.reset();
+    }
 
+    // Asociar la función de realizarMatricula al evento de clic del botón
+    document.getElementById('realizarMatricula').addEventListener('click', realizarMatricula);
+
+    // Mostrar la lista inicial de matrículas al cargar la página
+    mostrarListaMatriculas();
+});
