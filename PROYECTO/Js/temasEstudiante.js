ab0fdaba92b59bbf2895390cf6aec2a8495741ca
Juanchosky <142184377+Juanchosky@users.noreply.github.com>
2023-11-23 00:02:25 -0500

    Add files via upload

--- PROYECTO/Js/temasEstudiante.js
+++ PROYECTO/Js/temasEstudiante.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     cargarTemasEstudiante();
+    actualizarProgresoTemas();
+
 });
 
 function cargarTemasEstudiante() {
@@ -12,6 +14,8 @@ function cargarTemasEstudiante() {
 
     // Obtener la lista de temas del localStorage o inicializar un array vacío
     const temas = obtenerTemas();
+    const calificaciones = obtenerCalificacionesEstudiante(); // Asegúrate de tener esta función implementada
+
 
     // Limpiar el contenido actual de la sección
     misTemasDiv.innerHTML = '';
@@ -26,6 +30,8 @@ function cargarTemasEstudiante() {
             temasCurso.forEach(tema => {
                 const temaDiv = document.createElement('div');
                 temaDiv.innerHTML = `<p>${tema.nombre}</p>`;
+                const progreso = calcularProgreso(tema.id, calificaciones);
+                agregarProgreso(temaDiv, progreso);
                 misTemasDiv.appendChild(temaDiv);
             });
         } else {
@@ -35,6 +41,83 @@ function cargarTemasEstudiante() {
         misTemasDiv.innerHTML = '<p>No hay temas disponibles.</p>';
     }
 }
+
+
+
+function calcularProgreso(temaId, calificaciones) {
+    // Obtener las calificaciones asociadas al tema
+    const calificacionesTema = calificaciones.filter(calificacion => calificacion.temaId === temaId);
+
+    // Verificar si hay calificaciones para el tema
+    if (calificacionesTema.length > 0) {
+        // Calcular el progreso basándose en las calificaciones existentes
+        const notasCalificadas = calificacionesTema.reduce((acumulador, calificacion) => {
+            if (calificacion.nota) {
+                return acumulador + 1;
+            }
+            return acumulador;
+        }, 0);
+
+        const progreso = (notasCalificadas / 4) * 100; // Suponiendo 4 notas por tema
+        return progreso.toFixed(2); // Redondear a 2 decimales
+    }
+
+    return 0; // Si no hay calificaciones, el progreso es 0
+}
+
+function agregarProgreso(temaDiv, progreso) {
+    // Agregar la barra de progreso al div del tema
+    const progresoDiv = document.createElement('div');
+    progresoDiv.innerHTML = `<progress value="${progreso}" max="100"></progress>`;
+    temaDiv.appendChild(progresoDiv);
+}
+
+function obtenerCalificacionesEstudiante() {
+    // Implementación para obtener las calificaciones del estudiante, por ejemplo:
+    const loggedInUser = JSON.parse(localStorage.getItem('logged_in_user_data')) || {};
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const estudiante = users.find(user => user.id_number === loggedInUser.id_number);
+
+    if (estudiante && estudiante.calificaciones && Array.isArray(estudiante.calificaciones)) {
+        return estudiante.calificaciones;
+    }
+
+    return [];
+}
+
+
+
+
+function actualizarProgresoTemas() {
+    const loggedInUser = JSON.parse(localStorage.getItem('logged_in_user_data')) || {};
+
+    if (!loggedInUser.id_number) {
+        alert('No se encontró información del estudiante.');
+        return;
+    }
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const estudiante = users.find(user => user.id_number === loggedInUser.id_number);
+
+    if (estudiante && estudiante.calificaciones) {
+        const temasConProgreso = document.querySelectorAll('.tema-con-progreso');
+
+        temasConProgreso.forEach(temaDiv => {
+            const nombreTema = temaDiv.getAttribute('data-tema');
+            const progresoElement = temaDiv.querySelector('.progreso-tema');
+
+            // Verifica si hay calificación para este tema
+            if (estudiante.calificaciones.hasOwnProperty(nombreTema) && estudiante.calificaciones[nombreTema] !== '') {
+                progresoElement.value = 100; // Si hay calificación, el progreso es 100%
+            } else {
+                progresoElement.value = 0; // Si no hay calificación, el progreso es 0%
+            }
+        });
+    } else {
+        alert('No se encontraron calificaciones para el estudiante.');
+    }
+}
+
 
 function obtenerTemas() {
     // Implementación para obtener los temas, por ejemplo:
